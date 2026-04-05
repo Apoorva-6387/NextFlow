5fad7ea01c782b03155d530c8cf8cebd1c7dce8e
Apoorva-6387 <apoorva6387@gmail.com>
2026-04-05 17:38:50 +0530

    Read Connected Input in LLM Node

--- src/components/workflow/NodeText.tsx
+++ src/components/workflow/NodeText.tsx
@@ -1,26 +1,29 @@
 "use client";
 
 import { Handle, Position } from "reactflow";
+import { useState } from "react";
 
-export default function NodeText({ data }: any) {
+export default function NodeText({ id, data }: any) {
+  const [text, setText] = useState("");
+
+  // Save text in node data
+  data.text = text;
+
   return (
     <div className="bg-[#1e293b] text-white p-3 rounded-xl border border-purple-500 shadow-lg w-48">
 
-      {/* Title */}
       <div className="text-sm font-bold mb-2 text-purple-300">
         📝 Text Node
       </div>
 
-      {/* Input */}
       <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
         placeholder="Enter text..."
         className="w-full p-2 rounded bg-black text-white text-xs outline-none"
       />
 
-      {/* Handles */}
-      <Handle type="target" position={Position.Left} />
       <Handle type="source" position={Position.Right} />
-
     </div>
   );
 }
